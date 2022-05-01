97a5124386a1691862b9a06ae5acb7eb8476048a
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2022-05-01 16:40:22 +0700

    🏠 Type Approximation

--- type_set_test.go
+++ type_set_test.go
@@ -5,8 +5,10 @@ import (
 	"testing"
 )
 
+type Age int
+
 type Number interface {
-	int | int8 | int16 | int32 | int64 |
+	~int | int8 | int16 | int32 | int64 |
 		float32 | float64
 }
 
@@ -22,4 +24,5 @@ func TestMin(t *testing.T) {
 	assert.Equal(t, 100, Min[int](100, 200))
 	assert.Equal(t, int64(100), Min[int64](int64(100), int64(200)))
 	assert.Equal(t, float64(100), Min[float64](float64(100), float64(200)))
+	assert.Equal(t, Age(100), Min[Age](Age(100), Age(200)))
 }
