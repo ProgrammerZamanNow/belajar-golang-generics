46f41b4be812a9418ceefe95061f294eff52ef9d
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2022-05-01 16:04:55 +0700

    :dancer: Type Inheritance

---
+++ type_inheritance_test.go
@@ -0,0 +1,53 @@
+package belajar_golang_generics
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type Employee interface {
+	GetName() string
+}
+
+func GetName[T Employee](parameter T) string {
+	return parameter.GetName()
+}
+
+type Manager interface {
+	GetName() string
+	GetManagerName() string
+}
+
+type MyManager struct {
+	Name string
+}
+
+func (m *MyManager) GetName() string {
+	return m.Name
+}
+
+func (m *MyManager) GetManagerName() string {
+	return m.Name
+}
+
+type VicePresident interface {
+	GetName() string
+	GetVicePresidentName() string
+}
+
+type MyVicePresident struct {
+	Name string
+}
+
+func (m *MyVicePresident) GetName() string {
+	return m.Name
+}
+
+func (m *MyVicePresident) GetVicePresidentName() string {
+	return m.Name
+}
+
+func TestGetName(t *testing.T) {
+	assert.Equal(t, "Eko", GetName[Manager](&MyManager{Name: "Eko"}))
+	assert.Equal(t, "Eko", GetName[VicePresident](&MyVicePresident{Name: "Eko"}))
+}
