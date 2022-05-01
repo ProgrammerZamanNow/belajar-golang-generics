af56e151427bd6bf6ba3d1b57a21ab4b1616e5fc
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2022-05-01 17:15:19 +0700

    :boom: Generic Interface

---
+++ generic_interface_test.go
@@ -0,0 +1,37 @@
+package belajar_golang_generics
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type GetterSetter[T any] interface {
+	GetValue() T
+	SetValue(value T)
+}
+
+func ChangeValue[T any](param GetterSetter[T], value T) T {
+	param.SetValue(value)
+	return param.GetValue()
+}
+
+type MyData[T any] struct {
+	Value T
+}
+
+func (d *MyData[T]) GetValue() T {
+	return d.Value
+}
+
+func (d *MyData[T]) SetValue(value T) {
+	d.Value = value
+}
+
+func TestGenericInterface(t *testing.T) {
+	myData := MyData[string]{}
+
+	result := ChangeValue[string](&myData, "Eko")
+
+	assert.Equal(t, "Eko", result)
+	assert.Equal(t, "Eko", myData.Value)
+}
